3baf9cbb6f7b878945eb34524c897e4a413aafbd
Nick Smith <nick.smith@cern.ch>
2025-08-19 21:48:51 -0500

    Use auto-version

--- src/rosetta/__init__.py
+++ src/rosetta/__init__.py
@@ -1,7 +1,6 @@
 __author__ = ('Adam Falkowski, Benjamin Fuks, Kentarou Mawatari, Ken Mimasu, '
               'Francesco Riva & Veronica Sanz')
-__version__ = '2.1'
-__date__ = '13/03/2017'
+__date__ = '(undefined)'
 __url__ = 'http://rosetta.hepforge.org'
 
 
@@ -10,3 +9,4 @@ from .internal.machinery import bases as implemented_bases
 from .internal import session, settings
 
 from .bases import *
+from ._version import __version__
